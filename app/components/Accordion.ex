76f5f3df475120bb1a06897769f0e5335ddeba8e
JoshC <ccgjosh@gmail.com>
2026-01-09 11:39:49 -0500

    Add examples

---
+++ app/components/Accordion.ex
@@ -0,0 +1,53 @@
+defmodule HologramSkeleton.Components.Accordion do
+  use Hologram.Component
+
+  prop :title, :string, default: ""
+  prop :collapsed, :boolean, default: false
+  prop :class, :string, default: "border border-gray-200 rounded-lg"
+
+  prop :button_class, :string,
+    default:
+      "flex w-full items-center justify-between gap-x-4 p-4 text-left bg-neutral/10 hover:bg-neutral/20 rounded-t cursor-pointer"
+
+  prop :title_class, :string, default: "text-lg font-medium text-gray-800"
+  prop :content_class, :string, default: "p-4"
+
+  def init(props, component, _server) do
+    component
+    |> put_state(:collapsed, props.collapsed)
+  end
+
+  def template do
+    ~HOLO"""
+    <div class={@class}>
+      <div id={"accordion-#{@cid}"} class="group {if @collapsed do "closed" else "open" end}">
+        <button
+          class={@button_class}
+          aria-controls={"accordion-#{@cid}-collapse"}
+          aria-expanded={@collapsed}
+          $click={:toggle}>
+            <h2 class={@title_class}>
+              {@title}
+            </h2>
+            <span class="-rotate-90 group-[.open]:rotate-0 transition-transform duration-300">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="square" stroke-width="2" d="M17.5 9.5L12 15L6.5 9.5"/></svg>
+            </span>
+        </button>
+        <div id={"accordion-#{@cid}-collapse"} class="w-full overflow-hidden transition-all duration-300 max-h-0 group-[.open]:max-h-none" aria-labelledby={"optiongroup-#{@cid}"} role="region">
+          <div class={@content_class}>
+            <slot />
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  def action(:toggle, _params, component) do
+    new_collapsed = !component.state.collapsed
+
+    component
+    |> put_state(:collapsed, new_collapsed)
+  end
+
+end
---
+++ app/components/Block.ex
@@ -0,0 +1,43 @@
+defmodule HologramSkeleton.Components.Block do
+  use Hologram.Component
+
+  def init(_props, component, _server) do
+    component
+    |> put_state(:age, nil)
+  end
+
+  def template do
+    ~HOLO"""
+    <div class="p-4 border border-gray-300 rounded-md shadow-sm">
+      <slot />
+
+      {%if @age}
+        <p>Your age is: {@age}</p>
+        <button
+          class="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
+          $click="clear_age">
+          Clear Age
+        </button>
+      {/if}
+
+      <HologramSkeleton.Components.Accordion2 cid="nested-with-event-passing" title="Accordion Example 3" parent_cid={@cid}>
+        <form $submit="form_submitted" class="space-y-4">
+          <div>
+            <label for="age" class="block text-sm font-medium text-gray-700">Age</label>
+            <input type="text" name="age" id="age" required class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2" />
+          </div>
+          <button type="submit" class="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700">Submit</button>
+        </form>
+      </HologramSkeleton.Components.Accordion2>
+    </div>
+    """
+  end
+
+  def action(:form_submitted, %{event: %{"age" => age}}, component) do
+    put_state(component, :age, age)
+  end
+
+  def action(:clear_age, _params, component) do
+    put_state(component, :age, nil)
+  end
+end
