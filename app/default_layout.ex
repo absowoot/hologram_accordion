19fb8b96d86e92480b73e892c101b6a8030a09a2
JoshC <ccgjosh@gmail.com>
2026-05-29 12:31:50 -0400

    Add footer + links to default layout

--- app/default_layout.ex
+++ app/default_layout.ex
@@ -12,6 +12,25 @@ defmodule HologramSkeleton.DefaultLayout do
       </head>
       <body>
         <slot />
+
+        <footer class="bg-primary">
+          <div class="container py-6 lg:py-10 mx-auto">
+            <div class="flex">
+              <div class="flex flex-col w-3/4">
+                <h3 class="text-secondary font-bold text-4xl uppercase tracking-wider mb-3">
+                  Links
+                </h3>
+                <nav aria-label="Secondary">
+                  <ul class="text-xl uppercase tracking-wide flex flex-col lg:flex-row lg:flex-wrap font-gothic font-normal">
+                    <li class="lg:w-1/3 lg:pr-8 mt-2 lg:mb-4">
+                      <a href="/about-us" class="text-white">About Us</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
     """
--- config/config.exs
+++ config/config.exs
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.1.7",
+  version: "3.4.3",
   hologram_skeleton: [
     args: ~w(
       --config=tailwind.config.js
